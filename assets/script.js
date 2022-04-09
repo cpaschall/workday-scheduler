20bb20243e49a877e59b80e6ac8d54d7e1dc09b8
cpaschall <cpaschall216@gmail.com>
2022-04-09 10:03:36 -0400

    refactoring js and css

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,23 @@ var containerDiv = $(".container");
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 
+// createing an array that startes with blank strings
 var scheduleArray = ["","","","","","","","","",];
-
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"))
+// function is called initially to render list items associated with each business hour.  All elements are added in a for loop which included an IF statement to determine the hour of the day.
 function renderSchedule(){
-    containerDiv.append($("<h2>"))
-    $("h2").text(moment().format("dddd, MMMM Do YYYY, h:mm a"))
+    // containerDiv.append($("<h2>"))
+    // $("h2").text(moment().format("dddd, MMMM Do YYYY, h:mm a"))
     containerDiv.append($("<ul>"));
     for(var i = 0; i < scheduleArray.length; i++) {
         if (i >= 0 && i < 3) {
-            $("ul").append($(`<li><span>${i+9}:00 AM</span><textarea id="text-${i}" data-time=${i}></textarea><button id="btn-${i}"><span class="emoji">💾</span></button></li>`));
+            $("ul").append($(`<li><span class="each-hour"><p>${i+9}:00 AM</p></span><textarea id="text-${i}" data-time=${i} placeholder="TO-DO"></textarea><button id="btn-${i}"><i class="fa-solid fa-download emoji"></i></button></li>`));
             renderScheduleItems(i);
         }else if(i === 3) {
-            $("ul").append($(`<li><span>${i+9}:00 PM</span><textarea id="text-${i}" data-time=${i}></textarea><button id="btn-${i}"><span class="emoji">💾</span></button></li>`));
+            $("ul").append($(`<li><span class="each-hour"><p>${i+9}:00 PM</p></span><textarea id="text-${i}" data-time=${i} placeholder="TO-DO"></textarea><button id="btn-${i}"><i class="fa-solid fa-download emoji"></i></button></li>`));
             renderScheduleItems(i);
         }else if (i > 3 && i < 9) {
-            $("ul").append($(`<li><span>${i-3}:00 PM</span><textarea id="text-${i}" data-time=${i}></textarea><button id="btn-${i}"><span class="emoji">💾</span></button></li>`));
+            $("ul").append($(`<li><span class="each-hour"><p>${i-3}:00 PM</p></span><textarea id="text-${i}" data-time=${i} placeholder="TO-DO"></textarea><button id="btn-${i}"><i class="fa-solid fa-download emoji"></i></button></li>`));
             renderScheduleItems(i);
         }
     }
@@ -36,15 +38,37 @@ function renderScheduleItems(index) {
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 function currentTime() {
-    $("textarea").css("background-color", function(index) {
+    $("textarea").css("background", function(index) {
         if((index + 9) === parseInt(moment().format("H"))){
-            return "#FC4F4F"
+            return "var(--li-bg-on)"
         } 
         if ((index + 9)> moment().format("H")){
-            return "#80ED99"
+            return "var(--li-bg-after)"
         }
         if ((index + 9)< moment().format("H")){
-            return "#E2DCD5"
+            return "var(--li-bg-before)"
+        }   
+    });
+    $(".each-hour").css("background", function(index) {
+        if((index + 9) === parseInt(moment().format("H"))){
+            return "#28FFBF"
+        } 
+        if ((index + 9)> moment().format("H")){
+            return "#85F4FF"
+        }
+        if ((index + 9)< moment().format("H")){
+            return "#1597BB"
+        }   
+    });
+    $("button").css("background", function(index) {
+        if((index + 9) === parseInt(moment().format("H"))){
+            return "#F5FDB0"
+        } 
+        if ((index + 9)> moment().format("H")){
+            return "#B8FFF9"
+        }
+        if ((index + 9)< moment().format("H")){
+            return "#8FD6E1"
         }   
     });
 }
